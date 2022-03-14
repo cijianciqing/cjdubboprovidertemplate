dbd94555216d8d526ee95cddc5ae691d0afe98a1
cjcq <425430798@qq.com>
2022-03-14 13:35:39 +0800

    Initial commit

---
+++ src/test/java/com/dubbo/cjdubboprovidertemplate/CjdubboprovidertemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubbo.cjdubboprovidertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjdubboprovidertemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
